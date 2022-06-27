145ee46d570548567f6dcbdd3294ed836b28f83f
Pedro Lucas <p.leopoldino03@gmail.com>
2022-06-27 14:50:49 -0300

    ajuste

--- core/views.py
+++ core/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-<<<<<<< HEAD
 
 from django.http import HttpResponse
 
@@ -7,16 +6,11 @@ def index(request):
     return HttpResponse('<h1>Página inicial</h1>')
 # Create your views here.
 
-=======
->>>>>>> 5ad21d291cfaaf52bb8d6e4e847beb51f63f2ce6
 from multiprocessing import context
 
 def home(request):
     return render(request, 'index.html')
-<<<<<<< HEAD
 
-=======
->>>>>>> 5ad21d291cfaaf52bb8d6e4e847beb51f63f2ce6
 def login(request):
     return render(request, 'login.html')
 def cadastro(request):
